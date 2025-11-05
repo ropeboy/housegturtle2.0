da5072f16ab8445a3b0e38cf4fc84beabf943154
robeboy <petar.resanovic@gmail.com>
2025-11-05 18:46:10 +0100

    changing direction

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 from gturtle import *
+
 t = makeTurtle()
-t.setPenWidth(4)
-t.setPenColor("red")
-#t.hideTurtle()
+t.hideTurtle()
 
-w = 120
-h = 120
-x = -w//2
-y = h//2
-
+p = 8
+w = 240
+h = 240
+x = -w // 2
+y = h // 2
 c = 0
-p = 4
 d = 0
 
-setPos(x, y)
+t.setPenWidth(p)
+t.setPenColor("red")
+
+t.setPos(x, y)
 t.right(90)
-repeat h//p:
-    repeat w//p:
-        t.forward(p)
-        x = x + p
-        if abs(x) < (c*4):
-            setPenColor("black")
-        else:
-            setPenColor("red")
-    if d < 15:
-        c = c + 1
-        d = d + 1
-    if c >= 15:
-        c = c - 2
-    y = y - p
-    x = -w//2
-    t.setPos(-w//2, y)
+repeat
+h // p:
+repeat
+w // p:
+t.forward(p)
+x = x + p
+if abs(x) < (c * p):
+    setPenColor("black")
+else:
+    setPenColor("red")
+    print(c)
+
+if d < 15:
+    c = c + 1
+    d = d + 1
+if c >= 15:
+    c = c - 2
+y = y - p
+x = -w // 2
+t.setPos(-w // 2, y)
+
+
+
+
