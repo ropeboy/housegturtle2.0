533332948385855ab1eb9629a27a888d41b30827
ropeboy <124436421+ropeboy@users.noreply.github.com>
2025-11-06 11:22:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
-from gturtle import *
-
-t = makeTurtle()
-t.hideTurtle()
+from gturtle import* 
+t = makeTurtle
 
 p = 8
 w = 240
 h = 240
-x = -w // 2
-y = h // 2
+x = -w//2
+y = h//2
 c = 0
 d = 0
 
-t.setPenWidth(p)
+t = setPenWidth(p)
 t.setPenColor("red")
-
 t.setPos(x, y)
 t.right(90)
-repeat
-h // p:
-repeat
-w // p:
-t.forward(p)
-x = x + p
-if abs(x) < (c * p):
-    setPenColor("black")
-else:
-    setPenColor("red")
-    print(c)
+repeat h//p:
+    repeat w//p:
+        forward(p)
+        x = x + p
+        if abs(x) < (c*p):
+            t.setPenColor("black")
+        else:
+            t.setPenColor("red")
+    if d < 15:
+        d = d + 1
+        c = c + 1
+    if c >= 15:
+        c = c - 2
 
-if d < 15:
-    c = c + 1
-    d = d + 1
-if c >= 15:
-    c = c - 2
-y = y - p
-x = -w // 2
-t.setPos(-w // 2, y)
+    y = y - p
+    x = -w//2
+    setPos(-w//2, y)
 
 
 
